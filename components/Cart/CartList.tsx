c71d9b82da6e1834f042c948d9e12798a81e343a
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-07 15:28:26 -0300

    set 'redirect' link on CartList component

--- components/Cart/CartList.tsx
+++ components/Cart/CartList.tsx
@@ -5,6 +5,7 @@ import CardItem from './CartItem';
 import EmptyCart from './EmptyCart';
 import CartClientCredit from './CartClientCredit';
 import CartFinalPrice from './CartFinalPrice';
+import Link from 'next/link';
 const CartList = () => {
 	const {
 		selectedProducts,
@@ -18,16 +19,26 @@ const CartList = () => {
 				<EmptyCart />
 			) : (
 				<article className="flex flex-col justify-between p-2">
-					<div className=' flex flex-col container mx-auto mt-6"'>
+					<h2 className="text-center text-3xl  mt-2 font-bold mb-2">
+						Mis productos
+					</h2>
+					<div className='flex flex-col container mx-auto mt-6"'>
 						{selectedProducts.map((product, index) => (
-							<CardItem product={product} productIndex={index} key={index} />
+							<CardItem
+								product={product}
+								productIndex={index}
+								key={index}
+							/>
 						))}
 					</div>
 
 					<div className="mt-4 flex flex-col">
 						<CartFinalPrice finalPrice={getTotalPrice()} />
 						<CartClientCredit clientAmount={clientCredit} />
-						<div className=" mt-3 container mx-auto flex flex-row justify-end w-full  items-end">
+						<div className=" mt-3 container mx-auto flex flex-row justify-between w-full  items-end">
+							<Link href="/products">
+								<a>Volver atrás</a>
+							</Link>
 							<Button onClick={() => performPayment()}>
 								Realizar compra
 							</Button>
